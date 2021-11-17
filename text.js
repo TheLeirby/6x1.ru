aaa5bddb5e1a6d1edffe92af74100311164fc4c0
Vasiliy <88965041+TheLeirby@users.noreply.github.com>
2021-11-17 13:50:01 +0300

    Update text.js

--- text.js
+++ text.js
@@ -33,3 +33,18 @@ const text = [
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+  window.onload = function() {
+    setInterval(function() {
+      // Seconds
+      var seconds = new Date().getSeconds();
+      document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+  
+      // Minutes
+      var minutes = new Date().getMinutes();
+      document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
+  
+      // Hours
+      var hours = new Date().getHours();
+      document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+    }, 1000);
+  }
